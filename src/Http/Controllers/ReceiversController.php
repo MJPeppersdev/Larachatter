287e38ecd5ea2c7608eae312ee37f1e94ff56af8
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-11-30 18:55:33 -0800

    Create ReceiversController.php

---
+++ src/Http/Controllers/ReceiversController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Launcher\Larachatter\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Launcher\Larachatter\Repositories\UserRepository;
+
+class ReceiversController extends Controller
+{
+    /**
+     *  Create a new controller instance.
+     * 
+     *  @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     *  Search for receivers.
+     * 
+     *  @param Request      $request
+     *  @param UserRepository $userRepository
+     * 
+     *  @return \Illuminate\Http\Response|\Illuminate\Contracts\Routing\ResponseFactory
+     */
+    public function search(Request $request, UserRepository $userRepository): Response
+    {
+        if (($query = $request->input('q')) === null) {
+            return response([
+                'hits' => [],
+                'total' => 0,
+            ]);
+        }
+        
+        $paginator = $userRepository->search($query);
+
+        $result = [
+            'total' => $paginator->total(),
+            'hits'  => $paginator->items(),
+        ];
+
+        return response($result);
+    }
+}
